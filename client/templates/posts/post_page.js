7f7c4b4710925ff68190567c142e761835cd723c
Joonas <joonas.yoon@gmail.com>
2015-09-05 17:19:38 +0900

    Customized Comments View

--- client/templates/posts/post_page.js
+++ client/templates/posts/post_page.js
@@ -1,7 +1,10 @@
 Template.postPage.helpers({
 	ownPost: function() {
 		return this.userId === Meteor.userId();
-	},
+	}
+});
+
+Template.postPageComments.helpers({
 	comments: function() {
 		return Comments.find({postId: this._id});
 	}
