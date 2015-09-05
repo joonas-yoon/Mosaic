6af75e920414044e066d6a1f6e635f1d000af833
Joonas <joonas.yoon@gmail.com>
2015-09-05 16:21:01 +0900

    Added views for comment on List page

--- client/templates/posts/post_page.js
+++ client/templates/posts/post_page.js
@@ -1,5 +1,8 @@
 Template.postPage.helpers({
 	ownPost: function() {
 		return this.userId === Meteor.userId();
+	},
+	comments: function() {
+		return Comments.find({postId: this._id});
 	}
 });
--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -3,3 +3,9 @@ Template.postsList.helpers({
     return Posts.find({}, {sort: {title: 1, submitted: -1}});
   }
 });
+
+Template.postItem.helpers({
+  commentsCount: function() {
+    return Comments.find({postId: this._id}).count();
+  }
+});
