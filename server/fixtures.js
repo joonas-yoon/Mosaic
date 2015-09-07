2223b5a0b142de03de568f74ed0af90bf791e3b0
Joonas <joonas.yoon@gmail.com>
2015-09-07 17:23:58 +0900

    Change users to able to sign-in

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,13 +1,23 @@
-if (Posts.find().count() === 0) {
+if (Posts.find().count() < 10) {
 
   var now = new Date().getTime();
 
   // create two users
   var tomId = Meteor.users.insert({
-    profile: {name: 'Tom Coleman'}
+    createdAt : new Date(),
+    services : {
+      password : { bcrypt : "\$2a\$10\$GENCwS2IQxujGBFoT1FShux2lrM7ZIivtzC4ZvXY9QYqF8qujMdP2" },
+      resume : { loginTokens : [ ] }
+    },
+    username: "Tom Coleman"
   });
   var sachaId = Meteor.users.insert({
-    profile: {name: 'Sacha Greif'}
+    createdAt : new Date(),
+    services : {
+      password : { bcrypt : "\$2a\$10\$GENCwS2IQxujGBFoT1FShux2lrM7ZIivtzC4ZvXY9QYqF8qujMdP2" },
+      resume : { loginTokens : [ ] }
+    },
+    username: "Sacha Greif"
   });
 
   var tom = Meteor.users.findOne(tomId);
@@ -16,7 +26,7 @@ if (Posts.find().count() === 0) {
   var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
     userId: sacha._id,
-    author: sacha.profile.name,
+    author: sacha.username,
     content: 'http://sachagreif.com/introducing-telescope/',
     submitted: new Date(now - 7 * 3600 * 1000),
     commentsCount: 2
@@ -25,7 +35,7 @@ if (Posts.find().count() === 0) {
   Comments.insert({
     postId: telescopeId,
     userId: tom._id,
-    author: tom.profile.name,
+    author: tom.username,
     submitted: new Date(now - 5 * 3600 * 1000),
     body: 'Interesting project Sacha, can I get involved?'
   });
@@ -33,7 +43,7 @@ if (Posts.find().count() === 0) {
   Comments.insert({
     postId: telescopeId,
     userId: sacha._id,
-    author: sacha.profile.name,
+    author: sacha.username,
     submitted: new Date(now - 3 * 3600 * 1000),
     body: 'You sure can Tom!'
   });
@@ -41,9 +51,21 @@ if (Posts.find().count() === 0) {
   Posts.insert({
     title: 'Meteor',
     userId: tom._id,
-    author: tom.profile.name,
+    author: tom.username,
     content: 'http://meteor.com',
     submitted: new Date(now - 10 * 3600 * 1000),
     commentsCount: 0
   });
+
+  for(var i=0; i < 12; ++i){
+  Posts.insert({
+    title: 'Post #'+i,
+    userId: tom._id,
+    author: tom.username,
+    content: '## Test for paging '+ i,
+    submitted: new Date(now - i * 3600 * 1000),
+    commentsCount: 0
+  });
+  }
+
 }
